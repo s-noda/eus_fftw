16ecd626feb6a7d35ca1d7b82031cd3505b2ce41
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2014-11-21 18:51:13 +0900

    add extern statement for euslisp defforeign

--- src/eus_soundtouch_lib.cpp
+++ src/eus_soundtouch_lib.cpp
@@ -7,9 +7,10 @@ using namespace soundtouch;
 soundtouch::SoundTouch soundTouch;
 soundtouch::SoundTouch* pSoundTouch = &soundTouch;
 
-short* short_sound_array_buf;
+SAMPLETYPE* short_sound_array_buf;
 int short_sound_array_buf_size=-1;
 
+extern "C" {
 int setParamSoundTouch(int samplerate, int channel, float tempo, float pitch, float rate){
   pSoundTouch->setSampleRate(samplerate);
   pSoundTouch->setChannels(channel);
@@ -18,41 +19,54 @@ int setParamSoundTouch(int samplerate, int channel, float tempo, float pitch, fl
   pSoundTouch->setRateChange(rate);
   return 1;
 }
+}
 
-int putSampleSoundTouch(int* array, int size, int channel){
-  int i;
+extern "C" {
+void checkSizeOfBuffer(int size){
   if ( size != short_sound_array_buf_size ){
     free(short_sound_array_buf);
-    short_sound_array_buf = (short *)malloc( sizeof(short) * size );
+    short_sound_array_buf = (SAMPLETYPE *)malloc( sizeof(SAMPLETYPE) * size );
     short_sound_array_buf_size = size ;
   }
+}
+}
+
+extern "C" {
+int putSampleSoundTouch(int* array, int size, int channel){
+  int i;
+  checkSizeOfBuffer(size);
   for ( i=0; i<size ; i++ ){
     short_sound_array_buf[i] = (short)array[i] ;
   }
-  return pSoundTouch->putSamples(short_sound_array_buf, size/channel);
+  pSoundTouch->putSamples(short_sound_array_buf, size/channel);
+  return (size/channel);
+}
 }
 
+extern "C" {
 int receiveSampleSoundTouch(int* array, int size, int channel){
   int i ,ret;
-  if ( size != short_sound_array_buf_size ){
-    free(short_sound_array_buf);
-    short_sound_array_buf = (short *)malloc( sizeof(short) * size );
-    short_sound_array_buf_size = size ;
-  }
+  checkSizeOfBuffer(size);
   ret = pSoundTouch->receiveSamples(short_sound_array_buf, size/channel);
   for ( i=0; i<size ; i++ ){
     array[i] = (int)short_sound_array_buf[i];
   }
   return ret ;
 }
-
-int flushSoundTouch(){
-  return pSoundTouch->flush();
 }
 
-int init(){
+extern "C" {
+int flushSoundTouch(){
+  pSoundTouch->flush();
+  return 1;
+}
+}
+
+extern "C" {
+int initSoundTouch(){
   setParamSoundTouch(8000,1,1.0f,1.0f,1.0f);
   flushSoundTouch();
   return 0;
+}
 }
 
