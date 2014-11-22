c6276ba4cebdab82e689d925e7c5776f939c333e
s-noda <s-noda@jsk.t.u-tokyo.ac.jp>
2014-11-22 10:51:16 +0900

    depend wav_util

--- src/eus_soundtouch_lib.cpp
+++ src/eus_soundtouch_lib.cpp
@@ -1,72 +1,71 @@
 
 #include <iostream>
 #include <soundtouch/SoundTouch.h>
+#include "eus_wav_util.cpp"
 
 using namespace soundtouch;
 
-soundtouch::SoundTouch soundTouch;
-soundtouch::SoundTouch* pSoundTouch = &soundTouch;
+static soundtouch::SoundTouch soundTouch;
+static soundtouch::SoundTouch* pSoundTouch = &soundTouch;
 
-SAMPLETYPE* short_sound_array_buf;
-int short_sound_array_buf_size=-1;
+// SAMPLETYPE* short_sound_array_buf;
+// int short_sound_array_buf_size=-1;
 
-extern "C" {
-int setParamSoundTouch(int samplerate, int channel, float tempo, float pitch, float rate){
-  pSoundTouch->setSampleRate(samplerate);
-  pSoundTouch->setChannels(channel);
-  pSoundTouch->setTempoChange(tempo);
-  pSoundTouch->setPitchSemiTones(pitch);
-  pSoundTouch->setRateChange(rate);
-  return 1;
-}
-}
+// void checkSizeOfBuffer(int size){
+//   if ( size <= short_sound_array_buf_size ){
+//     free(short_sound_array_buf);
+//     short_sound_array_buf = (SAMPLETYPE *)malloc( sizeof(SAMPLETYPE) * size );
+//     short_sound_array_buf_size = size ;
+//   }
+// }
 
 extern "C" {
-void checkSizeOfBuffer(int size){
-  if ( size != short_sound_array_buf_size ){
-    free(short_sound_array_buf);
-    short_sound_array_buf = (SAMPLETYPE *)malloc( sizeof(SAMPLETYPE) * size );
-    short_sound_array_buf_size = size ;
+  int setParamSoundTouch(int samplerate, int channel, float tempo, float pitch, float rate){
+    pSoundTouch->setSampleRate(samplerate);
+    pSoundTouch->setChannels(channel);
+    pSoundTouch->setTempoChange(tempo);
+    pSoundTouch->setPitchSemiTones(pitch);
+    pSoundTouch->setRateChange(rate);
+    return 1;
   }
 }
-}
 
 extern "C" {
-int putSampleSoundTouch(int* array, int size, int channel){
-  int i;
-  checkSizeOfBuffer(size);
-  for ( i=0; i<size ; i++ ){
-    short_sound_array_buf[i] = (SAMPLETYPE)array[i] ;
+  int putSampleSoundTouch(int* array, int size, int channel){
+    int i;
+    checkSizeOfBuffer(size);
+    for ( i=0; i<size ; i++ ){
+      short_sound_array_buf[i] = (SAMPLETYPE)array[i] ;
+    }
+    pSoundTouch->putSamples(short_sound_array_buf, size/channel);
+    return (size/channel);
   }
-  pSoundTouch->putSamples(short_sound_array_buf, size/channel);
-  return (size/channel);
-}
 }
 
 extern "C" {
-int receiveSampleSoundTouch(int* array, int size, int channel){
-  int i ,ret;
-  checkSizeOfBuffer(size);
-  ret = pSoundTouch->receiveSamples(short_sound_array_buf, size/channel);
-  for ( i=0; i<size ; i++ ){
-    array[i] = (int)short_sound_array_buf[i];
+  int receiveSampleSoundTouch(int* array, int size, int channel){
+    int i ,ret;
+    checkSizeOfBuffer(size);
+    ret = pSoundTouch->receiveSamples(short_sound_array_buf, size/channel);
+    for ( i=0; i<size ; i++ ){
+      array[i] = (int)short_sound_array_buf[i];
+    }
+    return ret ;
   }
-  return ret ;
-}
 }
 
 extern "C" {
-int flushSoundTouch(){
-  pSoundTouch->flush();
-  return 1;
-}
+  int flushSoundTouch(){
+    pSoundTouch->flush();
+    return 1;
+  }
 }
 
 extern "C" {
-int initSoundTouch(){
-  setParamSoundTouch(8000,1,1.0f,1.0f,1.0f);
-  flushSoundTouch();
-  return 0;
-}
+  int initSoundTouch(){
+    setParamSoundTouch(8000,1,1.0f,1.0f,1.0f);
+    flushSoundTouch();
+    return 0;
+  }
 }
 
